3375beb67a6e142dcad850c7d458a1d81a750bdf
ken7253 <ken7253@gmail.com>
2023-12-25 01:35:18 +0900

    画像の切り替え処理を実装

--- src/browser/graphics/components/SponsorRoll/index.tsx
+++ src/browser/graphics/components/SponsorRoll/index.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import { useEffect, useState } from "react";
 
 import { css } from "@emotion/css";
 
@@ -8,11 +9,34 @@ const mockImage = [
   "https://placehold.jp/404244/ffffff/300x300.png",
 ];
 
-export const SponsorRoll: FC = () => {
+type Props = {
+  intervalTime: number;
+};
+
+export const SponsorRoll: FC<Props> = ({ intervalTime = 5000 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrentIndex((prev) => {
+        return prev + 1 === mockImage.length ? 0 : prev + 1;
+      });
+    }, intervalTime);
+
+    return () => clearInterval(id);
+  });
+
   return (
     <div className={styles.container}>
-      {mockImage.map((image) => {
-        return <img className={styles.image} key={image} src={image}></img>;
+      {mockImage.map((image, index) => {
+        return (
+          <img
+            className={styles.image}
+            key={image}
+            src={image}
+            hidden={currentIndex !== index}
+          ></img>
+        );
       })}
     </div>
   );
@@ -28,5 +52,12 @@ const styles = {
     top: 0;
     left: 0;
     padding: 16px;
+    opacity: 1;
+    transition: opacity 500ms ease-in-out;
+
+    &[hidden] {
+      display: block;
+      opacity: 0;
+    }
   `,
 };

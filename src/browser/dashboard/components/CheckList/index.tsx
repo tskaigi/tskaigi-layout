ea11218ba27a0a979b8a527c3d911045c3f093f4
ken7253 <ken7253@gmail.com>
2024-03-28 00:22:02 +0900

    チェックリスト機能の実装

--- src/browser/dashboard/components/CheckList/index.tsx
+++ src/browser/dashboard/components/CheckList/index.tsx
@@ -1,18 +1,38 @@
-import { type FC } from "react";
+import type { FC, ChangeEvent } from "react";
+import { useState } from "react";
 
 import { Checkbox, Stack } from "@mui/material";
 
 type Props = {
   items: string[];
-  onReady?: () => void;
+  onChange?: (ready: boolean) => void;
 };
 
-export const CheckList: FC<Props> = ({ items }) => {
+export const CheckList: FC<Props> = ({ items, onChange }) => {
+  const [checkList, setCheckList] = useState(items.map(() => false));
+
+  const checkHandler = (_: ChangeEvent, index: number) => {
+    setCheckList((prev) => {
+      const before = prev.slice(0, index);
+      const after = prev.slice(index + 1);
+      const result = [...before, !prev[index], ...after];
+
+      if (onChange) {
+        onChange(result.every(Boolean));
+      }
+
+      return result;
+    });
+  };
+
   return (
     <Stack direction="column" spacing={0.5}>
-      {items.map((item) => (
+      {items.map((item, index) => (
         <label key={item}>
-          <Checkbox />
+          <Checkbox
+            checked={checkList[index]}
+            onChange={(e) => checkHandler(e, index)}
+          />
           {item}
         </label>
       ))}
--- src/browser/dashboard/views/sample.tsx
+++ src/browser/dashboard/views/sample.tsx
@@ -1,17 +1,66 @@
 import type { FC } from "react";
+import { useEffect, useState } from "react";
+import { match } from "ts-pattern";
 
 import { render } from "../../render";
 
 import { CheckList } from "../components/CheckList";
+import { Button, Stack } from "@mui/material";
+
+type Progress = "standby" | "during";
 
 const App: FC = () => {
+  const [progress, setProgress] = useState<Progress>("standby");
+  const [toNext, setToNext] = useState(false);
+
+  const progressUpdateHandler = () => {
+    setToNext(false);
+    match(progress)
+      .with("standby", () => setProgress("during"))
+      .with("during", () => setProgress("standby"))
+      .exhaustive();
+  };
+
+  useEffect(() => {
+    console.log(`progress update : ${progress}`);
+    // TODO: obs-websocketなどと接続する
+  }, [progress]);
+
   return (
-    <div>
-      <h2>開始前チェックリスト</h2>
-      <CheckList items={["hoge", "fuga", "piyo"]}></CheckList>
-      <h2>終了後チェックリスト</h2>
-      <CheckList items={["foo", "bar", "buzz"]}></CheckList>
-    </div>
+    <Stack spacing={2}>
+      {match(progress)
+        .with("standby", (pattern) => (
+          <>
+            <h2>開始前チェックリスト</h2>
+            <CheckList
+              items={["hoge", "fuga", "piyo"]}
+              onChange={setToNext}
+              key={pattern}
+            />
+          </>
+        ))
+        .with("during", (pattern) => (
+          <>
+            <h2>終了後チェックリスト</h2>
+            <CheckList
+              items={["foo", "bar", "buzz"]}
+              onChange={setToNext}
+              key={pattern}
+            />
+          </>
+        ))
+        .exhaustive()}
+      <Button
+        variant="contained"
+        disabled={!toNext}
+        onClick={progressUpdateHandler}
+      >
+        {match(progress)
+          .with("standby", () => "開始")
+          .with("during", () => "終了")
+          .exhaustive()}
+      </Button>
+    </Stack>
   );
 };
 

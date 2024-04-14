0d278b3558d9353c91d2dd7288e66b2520f2aeae
ken7253 <ken7253@gmail.com>
2024-04-14 12:02:29 +0900

    一旦処理を簡略化

--- src/browser/dashboard/views/components/TalkSummary/index.tsx
+++ src/browser/dashboard/views/components/TalkSummary/index.tsx
@@ -1,54 +1,20 @@
-import { FC, ChangeEventHandler, useState } from "react";
+import { type FC } from "react";
 
-import { useTalkStatus } from "./useTalkStatus";
+import { useReplicant } from "../../../../hooks/useReplicant";
+import { TimeTableSchema } from "../../../../schema/TimeTable";
 import { Stack, TextField } from "@mui/material";
 
-import { updateByIndex } from "../../util/updateByIndex";
-import { type TimeTable } from "../../../data/timeTable";
-
-type Track = keyof TimeTable;
-
 export const TalkSummary: FC = () => {
-  const [track, setTrack] = useState<Track>("trackOne");
-  const [trackIndex, setTrackIndex] = useState(0);
-  const [talkStatus, setTalkStatus] = useTalkStatus(track);
+  const { value } = useReplicant("time-table", TimeTableSchema);
 
-  console.log({ talkStatus, track });
-
-  const changeNameHandler: ChangeEventHandler<
-    HTMLInputElement | HTMLTextAreaElement
-  > = ({ target }) => {
-    if (talkStatus === undefined) return;
-    const updateItem: (typeof talkStatus)[number] = {
-      title: talkStatus[trackIndex].title,
-      speakerName: target.value,
-    };
-
-    setTalkStatus(updateByIndex(talkStatus, trackIndex, updateItem));
-  };
-
-  const changeTitleHandler: ChangeEventHandler<
-    HTMLInputElement | HTMLTextAreaElement
-  > = (event) => {
-    console.log(event);
-  };
+  console.log(value);
 
   return (
     <div>
       <h2>現在の発表</h2>
       <Stack gap={2}>
-        <TextField
-          label="発表者名"
-          variant="outlined"
-          value={talkStatus?.[trackIndex].speakerName}
-          onChange={changeNameHandler}
-        />
-        <TextField
-          label="タイトル"
-          variant="outlined"
-          value={talkStatus?.[trackIndex].title}
-          onChange={changeTitleHandler}
-        />
+        <TextField label="発表者名" variant="outlined" />
+        <TextField label="タイトル" variant="outlined" />
       </Stack>
     </div>
   );

1f77591ba5207d6a4a0176379203f94b715f5d39
ken7253 <ken7253@gmail.com>
2024-04-27 16:37:18 +0900

    スポンサー画像を更新

--- src/browser/graphics/img/sponsor/roll/index.ts
+++ src/browser/graphics/img/sponsor/roll/index.ts
@@ -1,7 +1,7 @@
-import ascend from "./ascend.png";
-import dwango from "./dwango_horizontal.png";
-import helpfeel from "./helpfeel_horizontal.png";
+import ascend from "./ascend.jpg";
+import dwango from "./dwango_horizontal.jpg";
+import helpfeel from "./helpfeel_horizontal.jpg";
 import leverages from "./leverages.jpg";
-import progate from "./progate_horizontal.png";
+import progate from "./progate_horizontal.jpg";
 
 export { ascend, dwango, helpfeel, leverages, progate };
--- src/browser/graphics/img/sponsor/static/index.ts
+++ src/browser/graphics/img/sponsor/static/index.ts
@@ -1,9 +1,19 @@
-import earthbrain from "./earthbrain_horizontal.png";
-import googlecloud from "./googlecloudjapan.png";
-import layerx from "./layerx_horizontal.png";
-import medley from "./medley.png";
-import timee from "./timee_horizontal.png";
-import toggle from "./toggle.png";
-import ubie from "./ubie_horizontal.png";
+import bitkey from "./bitkey_horizontal.jpg";
+import earthbrain from "./earthbrain_horizontal.jpg";
+import googlecloudjapan from "./googlecloudjapan.jpg";
+import layerx from "./layerx_horizontal.jpg";
+import medley from "./medley.jpg";
+import timee from "./timee_horizontal.jpg";
+import toggle from "./toggle.jpg";
+import ubie from "./ubie_horizontal.jpg";
 
-export { earthbrain, googlecloud, layerx, medley, timee, toggle, ubie };
+export {
+  bitkey,
+  earthbrain,
+  googlecloudjapan,
+  layerx,
+  medley,
+  timee,
+  toggle,
+  ubie,
+};

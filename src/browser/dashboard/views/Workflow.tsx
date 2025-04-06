37145010106e0fd55917f3ff4f1c7f8b889ca1a5
ken7253 <ken7253@gmail.com>
2025-04-06 18:38:11 +0900

    Schemaの指定が間違っていたので修正

--- src/browser/dashboard/views/Workflow.tsx
+++ src/browser/dashboard/views/Workflow.tsx
@@ -2,23 +2,20 @@ import { type FC, type ComponentProps } from "react";
 import { render } from "../../render";
 
 import { useReplicant } from "../../hooks/useReplicant";
-import { type TimeTable, TimeTableSchema } from "../../schema/TimeTable";
-import { type Progress, ProgressSchema } from "../../schema/Progress";
+import { TimeTableSchema } from "../../schema/TimeTable";
+import { ProgressSchema } from "../../schema/Progress";
 import { Workflow } from "./components/Workflow";
 import { hasNextTalk } from "./util/hasNextTalk";
 
 type WorkflowProps = ComponentProps<typeof Workflow>;
 
 const App: FC = () => {
-  const { value: progress, setValue: setProgress } = useReplicant<Progress>(
+  const { value: progress, setValue: setProgress } = useReplicant(
     "progress",
-    TimeTableSchema,
-  );
-
-  const { value: timeTable } = useReplicant<TimeTable>(
-    "time-table",
     ProgressSchema,
   );
+
+  const { value: timeTable } = useReplicant("time-table", TimeTableSchema);
 
   const progressUpdateHandler: WorkflowProps["onChangeProgress"] = (mode) => {
     if (!hasNextTalk(timeTable, progress)) return;
